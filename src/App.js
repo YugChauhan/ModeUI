6ea43fd90f47ea98af395c1ac2aadd2c34de36a4
yugchauhan <yugchauhan715@gmail.com>
2024-01-06 06:49:49 -0800

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import AnnounceBar from './components/AnnounceBar'
+import Navbar from './components/Navbar'
+import Herosection from './components/Herosection'
+import LogoCloud from './components/LogoCloud'
+import Feature from './components/Feature'
+import Component1 from './components/Component1'
+import Testimonial from './components/Testimonial'
+import Calltoaction from './components/Calltoaction'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AnnounceBar/>
+      <Navbar/>
+      <Herosection/>
+      <LogoCloud/>
+      <Feature/>
+      <Component1/>
+      <Testimonial/>
+      <Calltoaction/>
+      <Footer></Footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
